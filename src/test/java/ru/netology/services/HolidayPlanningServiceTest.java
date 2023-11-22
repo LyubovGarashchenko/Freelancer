7795dc9342342eada571732419d8e32973fd528e
Lyubov Garashchenko <lyubov-balabina@yandex.ru>
2023-11-23 00:51:22 +0300

    Csv

--- src/test/java/ru/netology/services/HolidayPlanningServiceTest.java
+++ src/test/java/ru/netology/services/HolidayPlanningServiceTest.java
@@ -1,24 +1,26 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 class HolidayPlanningServiceTest {
-    @Test
-    public void testHolidayPlanning1() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/freelancer.csv")
+    public void testHolidayPlanning1(int expected, int income, int expenses, int threshold) {
         HolidayPlanningService service = new HolidayPlanningService();
-        int actual = service.calculate(10_000, 3_000, 20_000);
-        int expected = 6;
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testHolidayPlanning2() {
-        HolidayPlanningService service = new HolidayPlanningService();
-        int actual = service.calculate(100_000, 600_000, 150_000);
-        int expected = 0;
+        int actual = service.calculate(income, expenses, threshold);
+//        int expected = 6;
 
         Assertions.assertEquals(expected, actual);
     }
 }
+ //   @Test
+ //   public void testHolidayPlanning2() {
+ //      HolidayPlanningService service = new HolidayPlanningService();
+//      int actual = service.calculate(100_000, 600_000, 150_000);
+//       int expected = 0;
+
+//        Assertions.assertEquals(expected, actual);
+//    }
+//}
